1e6bc495b6789034e15bbc430096f4e2b8f7252f
wahjay <hotan38@berkeley.edu>
2019-12-14 00:48:57 -0800

    added signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
+  #get 'static_pages/home'
 
-  match '/help', to: 'static_pages#help', :via => [:get, :post]
-  match '/about', to: 'static_pages#about', :via => [:get, :post]
-  match '/contact', to: 'static_pages#contact', :via => [:get, :post]
+  #match '/help', to: 'static_pages#help', :via => [:get, :post]
+  #match '/about', to: 'static_pages#about', :via => [:get, :post]
+  #match '/contact', to: 'static_pages#contact', :via => [:get, :post]
 
+  get '/help',  to: 'static_pages#help'
+  get '/about',  to: 'static_pages#about'
+  get '/contact',  to: 'static_pages#contact'
+  get '/signup',  to: 'users#new'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
