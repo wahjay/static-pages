818180766acf0d60a133c7cd26170514074c8ccb
wahjay <hotan38@berkeley.edu>
2019-12-15 16:42:57 -0800

    permanent persistence using cookies

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,11 +4,30 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def remember(user)
+    #this line creates a random remember_token and digest it and save to the database
+    user.remember
+    #use cookies to associate the user_id with the digested remember_token
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+
+  end
+
   def current_user
-    #rethrn the current logged-in user (if any)
-    if session[:user_id]
+    #rethrn the current logged-in user (if any) (temporary)
+    # this if statement means: “If session of user id exists (while setting user id to session of user id)"
+    if (user_id = session[:user_id])
       #get current_user if it's nil, else return as it is
       @current_user ||= User.find_by(id: session[:user_id])
+
+    #check cookie (permanent session)
+    elsif (user_id = cookies.signed[:user_id])
+      #'cookies.signed[:user_id]' automatically decrypt user_id and use it
+      user = User.find_by(id: cookies.signed[:user_id])
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in(user)
+        @current_user = user
+      end
     end
   end
 
@@ -17,8 +36,19 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # Forgets a persistent session
+  def forget(user)
+    #update the user.remember_digest to nil
+    user.forget
+    #remove the user_id from the cookies
+    cookies.delete(:user_id)
+    #remove correspoding remember_token from the cookies
+    cookies.delete(:remember_token)
+  end
+
   # Logs out the current user.
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, length: { maximum: 50 }
@@ -14,5 +16,29 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Remember a user in the database for use in persistent session.
+  def remember
+    #remember_token is virtual
+    #actually, there is no remember_token attribtue in User model
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user by updating the remember digest with nil
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
