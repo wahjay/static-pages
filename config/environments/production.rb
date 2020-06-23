9afcd91507c16cd13475cd38843ba52e6433af6e
wahjay <hotan38@berkeley.edu>
2020-06-22 23:10:31 -0700

    fixed account activation link directing to an unexpected page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'glacial-inlet-12153.herokuapp.com'
+  host = 'wahjay.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
